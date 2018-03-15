eb46ddc519b08165d26f9f55d1965c0b030b1f22
luoxin <goodmanluoxin@163.com>
2018-03-15 11:16:56 +0800

    展开全部

--- pages/infoLive/infoLive.js
+++ pages/infoLive/infoLive.js
@@ -5,14 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    IntroShowMore: false
+  },
+
+  bindPersonIntro: function(){
+    this.setData({
+      IntroShowMore: true
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      IntroShowMore: false
+    })
   },
 
   /**
